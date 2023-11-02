2733d1f45c6100a5a522e36e767fc9b5e0f4a73c
ziontee113 <ziontee113@gmail.com>
2023-11-02 17:25:41 +0700

    draft: stream json from API to Nui buffer

--- lua/ollama.nvim/init.lua
+++ lua/ollama.nvim/init.lua
@@ -1,9 +1,12 @@
 -- imports
+
 local curl = require("plenary.curl")
+local Popup = require("nui.popup")
 
 local M = {}
 
 -- connection info
+
 local ollama_port = 11434
 local ollama_url = string.format("http://localhost:%s/api/generate", ollama_port)
 
@@ -11,20 +14,69 @@ local ollama_url = string.format("http://localhost:%s/api/generate", ollama_port
 
 local test_parameters = {
     model = "zephyr",
-    prompt = "in 1 sentence, tell me the distance to the moon.",
+    prompt = "calculate distance in python using numpy",
 }
 
 local request_body = vim.json.encode(test_parameters)
 
 local cmd = string.format("curl -X POST %s -d '%s'", ollama_url, request_body)
 
+-- functions
+
+local popup = Popup({
+    position = "50%",
+    size = {
+        width = 80,
+        height = 40,
+    },
+    enter = true,
+    focusable = true,
+    zindex = 50,
+    relative = "editor",
+    border = {
+        padding = {
+            top = 2,
+            bottom = 2,
+            left = 3,
+            right = 3,
+        },
+        style = "rounded",
+        text = {
+            top = " I am top title ",
+            top_align = "center",
+            bottom = "I am bottom title",
+            bottom_align = "left",
+        },
+    },
+    buf_options = {
+        modifiable = true,
+        readonly = false,
+        filetype = "markdown",
+    },
+    win_options = {
+        winblend = 10,
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        wrap = true,
+        linebreak = true,
+    },
+})
+
+local append_str_to_end_of_buffer = function(bufnr, str)
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local last_line = lines[#lines]
+    local new_lines = vim.split(str, "\n")
+
+    vim.api.nvim_buf_set_text(bufnr, #lines - 1, #last_line, #lines - 1, #last_line, new_lines)
+end
+
 local call = function()
-    local data_tbl = {}
+    popup:mount()
 
     local job_id = vim.fn.jobstart(cmd, {
         on_stdout = function(_, data, _)
-            for _, str in ipairs(data) do
-                N(str)
+            for _, json_str in ipairs(data) do
+                local ok, decoded = pcall(vim.json.decode, json_str)
+                if ok then append_str_to_end_of_buffer(popup.bufnr, decoded.response) end
             end
         end,
         on_exit = function()
